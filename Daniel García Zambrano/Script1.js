7c7e33072083c691428594e1afee73dc418548cb
xdanxlei <55701145+xdanxlei@users.noreply.github.com>
2020-01-24 16:53:57 +0100

    Primera entrega

---
+++ Daniel García Zambrano/Script1.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var precio1 = parseInt(prompt("Introduce el precio 1: ", "0"));
+var precio2 = parseInt(prompt("Introduce el precio 2: ", "0"));
+alert("El precio es " + precio1 + " + " + precio2 + " = " + (precio1 + precio2) + ".");
+
+var parrafo = document.getElementById("demo").innerHTML;
+document.getElementById("demo").innerHTML += " ayuda.";
